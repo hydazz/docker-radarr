bff652bed20c059b80f57f3c52afbb2903dd558e
hydazz <53986978+hydazz@users.noreply.github.com>
2020-12-23 14:10:20 +1100

    testing new workflow

--- update_version_info.sh
+++ update_version_info.sh
@@ -3,12 +3,15 @@
 OVERLAY_VERSION=$(curl -sX GET "https://raw.githubusercontent.com/hydazz/docker-baseimage-alpine/main/version_info.json" | jq -r .overlay_version)
 OLD_OVERLAY_VERSION=$(cat version_info.json | jq -r .overlay_version)
 OLD_RADARR_RELEASE=$(cat version_info.json | jq -r .radarr_release)
+
 sed -i \
   -e "s/${OLD_OVERLAY_VERSION}/${OVERLAY_VERSION}/g" \
   -e "s/${OLD_RADARR_RELEASE}/${RADARR_RELEASE}/g" \
   README.md
+
 NEW_VERSION_INFO="overlay_version|radarr_release
 ${OVERLAY_VERSION}|${RADARR_RELEASE}"
+
 jq -Rn '
 ( input  | split("|") ) as $keys |
 ( inputs | split("|") ) as $vals |
