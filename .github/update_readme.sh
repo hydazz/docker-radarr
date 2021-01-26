04ae803719549c237cdebb2b9ba6b99b5bbf8baf
hydazz <53986978+hydazz@users.noreply.github.com>
2021-01-26 21:27:46 +1100

    changed update wording

--- .github/update_readme.sh
+++ .github/update_readme.sh
@@ -44,7 +44,7 @@ See [package_versions.txt](package_versions.txt) for a full list of the packages
 
 ## Upgrading Radarr
 
-To upgrade, all you have to do is pull our latest Docker image. We automatically check for Radarr updates daily so there may be some delay when an update is released to when the image is updated.
+To upgrade, all you have to do is pull the latest Docker image. We automatically check for Radarr updates daily so there may be some delay when an update is released to when the image is updated.
 
 ## Credits
 
